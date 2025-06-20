f42fdd2b82ecea8ddf39ef7fa65a5a5d8e7a00ce
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:32:01 +0000

    [automatic commit] 2025-06-20 02:32:01 UTC 122.148.227.45

--- part1/app.js
+++ part1/app.js
@@ -80,7 +80,7 @@ let db;
             CONSTRAINT unique_application UNIQUE (request_id, walker_id)
         );
 
-        CREATE TABLE WalkRatings (
+        CREATE TABLE IF NOT EXISTS WalkRatings (
             rating_id INT AUTO_INCREMENT PRIMARY KEY,
             request_id INT NOT NULL,
             walker_id INT NOT NULL,

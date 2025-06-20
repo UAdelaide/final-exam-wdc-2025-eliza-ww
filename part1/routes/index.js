be85b469fdaf4b6d7d0f96c74d476072b58a8af7
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:12:28 +0000

    [automatic commit] 2025-06-20 02:12:28 UTC 122.148.227.45

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -18,5 +18,14 @@ router.get('/api/walkers/summary', function(req, res, next) {
   //
 });
 
+// app.get('/', async (req, res) => {
+//   try {
+//     const [books] = await db.execute('SELECT * FROM books');
+//     res.json(books);
+//   } catch (err) {
+//     res.status(500).json({ error: 'Failed to fetch books' });
+//   }
+// });
+
 
 module.exports = router;

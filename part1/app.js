a589666667e867b8038a335d21e9644affd09bca
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:37:34 +0000

    [automatic commit] 2025-06-20 02:37:34 UTC 122.148.227.45

--- part1/app.js
+++ part1/app.js
@@ -113,11 +113,13 @@ let db;
 
     if (dogCount[0].count === 0) {
         await db.execute(`
-        INSERT INTO Dogs (name, size, owner_id)
-        VALUES ("Max", "medium",
-        (SELECT user_id FROM Users WHERE username = "alice123")),
-        ("Bella", "small",
-        (SELECT user_id FROM Users WHERE username = "carol123"));
+            INSERT INTO Dogs (name, size, owner_id)
+            VALUES ("Max", "medium",
+            (SELECT user_id FROM Users WHERE username = "alice123")),
+            ("Bella", "small",
+            (SELECT user_id FROM Users WHERE username = "carol123")),
+            ("Maple", "medium",
+            (SELECT user_id FROM Users WHERE username = "eliza"));
         `);
     }
 

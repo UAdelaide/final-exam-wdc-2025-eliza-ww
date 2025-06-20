35a835657152865c16f6a6edd2814a6b1a5903e3
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:27:26 +0000

    [automatic commit] 2025-06-20 02:27:26 UTC 122.148.227.45

--- part1/app.js
+++ part1/app.js
@@ -116,6 +116,49 @@ let db;
         ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted");
       `);
     }
+
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+
+    if (rows[0].count === 0) {
+      await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
+        ("bobwalker", "bob@example.com", "hashed456", "walker"),
+        ("carol123", "carol@example.com", "hashed789", "owner");
+
+        INSERT INTO Dogs (name, size, owner_id)
+        VALUES ("Max", "medium",
+        (SELECT user_id FROM Users WHERE username = "alice123")),
+        ("Bella", "small",
+        (SELECT user_id FROM Users WHERE username = "carol123"));
+
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        VALUES ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+        ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted");
+      `);
+    }
+
+        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+
+    if (rows[0].count === 0) {
+      await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
+        ("bobwalker", "bob@example.com", "hashed456", "walker"),
+        ("carol123", "carol@example.com", "hashed789", "owner");
+
+        INSERT INTO Dogs (name, size, owner_id)
+        VALUES ("Max", "medium",
+        (SELECT user_id FROM Users WHERE username = "alice123")),
+        ("Bella", "small",
+        (SELECT user_id FROM Users WHERE username = "carol123"));
+
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        VALUES ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+        ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted");
+      `);
+    }
+
   }
 
   catch (err) {

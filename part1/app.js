0e193f47160201e75a114e901bcc95384247488e
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:29:31 +0000

    [automatic commit] 2025-06-20 02:29:30 UTC 122.148.227.45

--- part1/app.js
+++ part1/app.js
@@ -137,12 +137,14 @@ let db;
 
 // Route to return books as JSON
 app.get('/', async (req, res) => {
-  try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
-  }
+    try {
+        const [books] = await db.execute('SELECT * FROM books');
+        res.json(books);
+    }
+
+    catch (err) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
 });
 
 app.use('/', indexRouter);

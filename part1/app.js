612f1a1882c740ff30b0b1b83bb43fd7450a4f66
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:26:24 +0000

    [automatic commit] 2025-06-20 02:26:24 UTC 122.148.227.45

--- part1/app.js
+++ part1/app.js
@@ -100,10 +100,20 @@ let db;
 
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
+        ("bobwalker", "bob@example.com", "hashed456", "walker"),
+        ("carol123", "carol@example.com", "hashed789", "owner");
+
+        INSERT INTO Dogs (name, size, owner_id)
+        VALUES ("Max", "medium",
+        (SELECT user_id FROM Users WHERE username = "alice123")),
+        ("Bella", "small",
+        (SELECT user_id FROM Users WHERE username = "carol123"));
+
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        VALUES ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+        ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted");
       `);
     }
   }

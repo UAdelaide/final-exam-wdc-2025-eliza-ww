4712b75d9069054576567fc9da8bc1f43e007a05
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 02:34:26 +0000

    [automatic commit] 2025-06-20 02:34:26 UTC 122.148.227.45

--- part1/question1-5.sql
+++ part1/question1-5.sql
@@ -16,14 +16,14 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
-("eliza", "eliza@example.com", "hashed699", "walker"),
-("gregoryHouseMD", "drHouse@example.com", "itscanis", "owner"),;
+("eliza", "eliza@example.com", "hashed699", "owner"),
+("gregoryHouseMD", "drHouse@example.com", "itslupus", "walker"),;
 
 INSERT INTO Dogs (name, size, owner_id)
 VALUES ("Max", "medium",
 (SELECT user_id FROM Users WHERE username = "alice123")),
 ("Bella", "small",
-(SELECT user_id FROM Users WHERE username = "carol123"));
+(SELECT user_id FROM Users WHERE username = "carol123")),;
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),

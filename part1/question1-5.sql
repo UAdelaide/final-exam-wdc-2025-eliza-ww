9846e36ca08056c56c617528c8745a677b56c365
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-20 01:57:06 +0000

    [automatic commit] 2025-06-20 01:57:06 UTC 122.148.227.45

--- part1/question1-5.sql
+++ part1/question1-5.sql
@@ -12,14 +12,16 @@
 -- A request for Bella at 2025-06-10 09:30:00 for 45 minutes at Beachside Ave, with status accepted.
 -- Three more walk requests with details of your choosing.
 
-INSERT INTO Users (username, email, password_hash, role) VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
+INSERT INTO Users (username, email, password_hash, role)
+VALUES ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner");
 
-INSERT INTO Dogs (name, size, owner_id) VALUES ("Max", "medium",
+INSERT INTO Dogs (name, size, owner_id)
+VALUES ("Max", "medium",
 (SELECT user_id FROM Users WHERE username = "alice123")),
 ("Bella", "small",
 (SELECT user_id FROM Users WHERE username = "carol123"));
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-VALUES ()
+VALUES ((SELECT ));
